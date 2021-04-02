670956007e971564ae25ac961b24b74163b6eaf9
João Macieira <jpfis2015@gmail.com>
2021-04-02 15:32:53 -0300

    metodo findAll no model recipes

--- src/app/models/Recipes.js
+++ src/app/models/Recipes.js
@@ -1,7 +1,13 @@
 const db = require('../../database/index');
 
 class Recipes {
+  async findAll() {
+    const rows = await db.query(`
+      SELECT * FROM receitas
+    `);
 
+    return rows;
+  }
 }
 
 module.exports = new Recipes();
