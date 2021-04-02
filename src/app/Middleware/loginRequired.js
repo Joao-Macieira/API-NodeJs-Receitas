a07b9613406b0f3cd92a18c35f4defb134e6316d
João Macieira <jpfis2015@gmail.com>
2021-04-02 17:25:35 -0300

    Sistema de autenticação de login

--- src/app/Middleware/loginRequired.js
+++ src/app/Middleware/loginRequired.js
@@ -15,10 +15,9 @@ module.exports = async (req, res, next) => {
   const data = jwt.verify(token, process.env.TOKEN_SECRET);
   const { id, login } = data;
 
-  const user = await User.findOne({
+  const user = await User.findById({
     where: {
       id,
-      login,
     },
   });
 
