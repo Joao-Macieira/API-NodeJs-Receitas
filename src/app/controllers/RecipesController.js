3171842c0612b90072150775398b376291bda2d7
João Macieira <jpfis2015@gmail.com>
2021-04-02 15:50:32 -0300

    Método store no controller de receitas

--- src/app/controllers/RecipesController.js
+++ src/app/controllers/RecipesController.js
@@ -6,6 +6,30 @@ class RecipesController {
 
     return response.json({ recipes });
   }
+
+  async store(request, response) {
+    const {
+      userId,
+      categoryId,
+      name,
+      preparationTime,
+      portions,
+      method,
+      ingredients,
+    } = request.body;
+
+    if (!userId) return response.json({ error: 'Dados inválidos' });
+
+    if (!categoryId) return response.json({ error: 'Selecione a categoria da sua receita' });
+
+    if (!method) return response.json({ error: 'Descreva coo preparar sua receita' });
+
+    const newRecipe = await Recipes.create(
+      userId, categoryId, name, preparationTime, portions, method, ingredients,
+    );
+
+    return response.json({ newRecipe });
+  }
 }
 
 module.exports = new RecipesController();
